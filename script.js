ab181b10e630dc2454d96c4873abbe26a3eca539
nilcoder09 <144077609+nilcoder09@users.noreply.github.com>
2023-10-04 17:20:20 -0500

    more updates

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-function sendEmail() {
-    const message = document.getElementById('message').value;
-  
-    // Replace 'your-email@example.com' with your actual email address
-    const toEmail = 'your-email@example.com';
-  
-    // Basic email sending using mailto (replace with a server-side solution in production)
-    window.location.href = `mailto:${toEmail}?subject=Job Application&body=${message}`;
-  }
-  
+document.addEventListener("DOMContentLoaded", function () {
+    const chatContainer = document.getElementById("chat-container");
+    const chatMessages = document.getElementById("chat-messages");
+    const userInput = document.getElementById("user-input");
+    const sendBtn = document.getElementById("send-btn");
+
+    sendBtn.addEventListener("click", function () {
+        const userMessage = userInput.value;
+
+        if (userMessage.trim() !== "") {
+            // Display user message
+            displayMessage(userMessage, "user");
+
+            // Simulate server response (in this case, send an email)
+            simulateServerResponse(userMessage);
+
+            // Clear input
+            userInput.value = "";
+        }
+    });
+
+    function displayMessage(message, sender) {
+        const messageDiv = document.createElement("div");
+        messageDiv.className = sender === "user" ? "user-message" : "bot-message";
+        messageDiv.textContent = message;
+        chatMessages.appendChild(messageDiv);
+
+        // Scroll to the bottom to show the latest message
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+
+    function simulateServerResponse(userMessage) {
+        // Simulate sending an email
+        const botMessage = `Your message "${userMessage}" has been sent!`;
+        displayMessage(botMessage, "bot");
+    }
+});
